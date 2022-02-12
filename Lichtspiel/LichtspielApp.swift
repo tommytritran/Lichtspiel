c2d08c5c1b66b2c2d5037c8d5fdb25631a12f6ab
Tommy Tran <tommytritran@gmail.com>
2022-02-12 17:17:53 +0100

    initial commit

---
+++ Lichtspiel/LichtspielApp.swift
@@ -0,0 +1,17 @@
+//
+//  LichtspielApp.swift
+//  Lichtspiel
+//
+//  Created by Tommy Tran on 12/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct LichtspielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
